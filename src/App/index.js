0195c495844261e7baf242f576977dc7f1eed631
Christopher Möller <chrtze@gmail.com>
2018-11-05 12:28:22 +0100

    feat(app): use custom login page with className

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,8 @@
 import React, { PureComponent } from 'react';
 import { Admin, Resource } from 'react-admin';
 
-import './App.css';
-
 import Layout from '../Layout';
+import Login from '../resources/Login';
 import strapiProvider from '../providers/strapi';
 import authProvider from '../providers/authProvider';
 import addUploadFeature from '../providers/addUploadFeature';
@@ -15,7 +14,7 @@ const uploadCapableDataProvider = addUploadFeature(strapiProvider);
 class App extends PureComponent {
   render() {
     return (
-      <Admin appLayout={Layout} authProvider={authProvider} dataProvider={uploadCapableDataProvider}>
+      <Admin appLayout={Layout} loginPage={Login} authProvider={authProvider} dataProvider={uploadCapableDataProvider}>
         <Resource name="articles" list={ArticleList} edit={ArticleEdit} create={ArticleCreate} icon={ArticleIcon} />
         <Resource name="tags" />
         <Resource name="organisations" edit={OrganisationEdit} icon={OrganisationIcon} />
---
+++ src/resources/Login/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Login } from 'react-admin';
+
+import './Login.css';
+
+export default () => (
+  <Login className="ml-login" />
+);
